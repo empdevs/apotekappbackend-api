c8da1b5273e186704e9f9f35c0a8273a971a9228
haekalarif <haekal.rozikin@kitameraki.com>
2022-06-01 21:28:24 +0700

    update 01-06-2022 21.28

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -218,67 +218,83 @@ export async function updateCategory(req, res){
     let user = req.body.updated_by;
     let timeNow = moment().format("YYYY-MM-DD hh:mm:ss");
 
-
-    try{
-
-            //data for response if success
-        let data = {
-
-            "name" : name,
-            "updated_at" : timeNow,
-            "updated_by" : user,
-        }
-
-        // console.log(data);
-
-        await Categories.update(data, {
-
-            "where" : {
-
-                "id" : id
-
-            }
-
-        });
-
-        // console.log("Success udpate data category...");
-
-        let responseData = {
-            
-            "success" : true,
-            "status" : res.statusCode,
-            "message" : "Success udpate data",
-            "data" : data
-
-        }
-
-        // console.log(responseData);
-
-        return res.status(res.statusCode).json(responseData);
-
-
-    }catch(error){
-
-        console.log("Failed udpate data category...");
-
-        let errorStr = JSON.stringify(error);
-
-        let errorToJson = JSON.parse(errorStr);
-
-        // console.log(errorToJson);
+    if(!name || name === "" || name.match(/^ *$/) !== null){
 
         let errorObj = {
 
             "error" : true,
             "status" : 401,
-            "message" : errorToJson.original.sqlMessage,
+            "message" : "Please insert category name",
 
         }
 
         // console.log(errorObj);
 
-        return res.status(401).json(errorObj);
+        return res.status(errorObj.status).json(errorObj);
+    
+    }else{
 
+        try{
+
+                //data for response if success
+            let data = {
+
+                "name" : name,
+                "updated_at" : timeNow,
+                "updated_by" : user,
+            }
+
+            // console.log(data);
+
+            await Categories.update(data, {
+
+                "where" : {
+
+                    "id" : id
+
+                }
+
+            });
+
+            // console.log("Success udpate data category...");
+
+            let responseData = {
+                
+                "success" : true,
+                "status" : res.statusCode,
+                "message" : "Success udpate data",
+                "data" : data
+
+            }
+
+            // console.log(responseData);
+
+            return res.status(res.statusCode).json(responseData);
+
+
+        }catch(error){
+
+            console.log("Failed udpate data category...");
+
+            let errorStr = JSON.stringify(error);
+
+            let errorToJson = JSON.parse(errorStr);
+
+            // console.log(errorToJson);
+
+            let errorObj = {
+
+                "error" : true,
+                "status" : 401,
+                "message" : errorToJson.original.sqlMessage,
+
+            }
+
+            // console.log(errorObj);
+
+            return res.status(401).json(errorObj);
+
+        }
     }
 
 }
